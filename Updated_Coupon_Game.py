adf1bd9ef503728177248d44bb08f46f8983b1e8
walternyamutamba-svg <walter.nyamutamba@aims.ac.rw>
2025-09-18 00:04:55 +0200

    Update Updated_Coupon_Game.py

--- Updated_Coupon_Game.py
+++ Updated_Coupon_Game.py
@@ -50,7 +50,7 @@ mean_simulation = simulate(n)
 
 st.subheader("📊 Results")
 st.write(f"**Analytic expected waiting time:** {analytic:.2f} draws")
-st.write(f"**Simulated average number of draws:** {mean_simulation:.2f} draws")
+#st.write(f"**Simulated average number of draws:** {mean_simulation:.2f} draws")
 
 # Optional: Let user recompute if they change n
 if st.button("Re-run Simulation"):
